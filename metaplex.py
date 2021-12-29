f5c37740d73cc1bdd3107104ad7de4fa46888160
Carlito <carl@ip-172-26-4-184.us-west-2.compute.internal>
2021-12-29 09:44:54 +0000

    save metaplex metadata

--- metaplex.py
+++ metaplex.py
@@ -5,69 +5,109 @@ import json
 def format_attributes(dict_attrs):
     attributes = []
     for attr in dict_attrs:
+        
         for k, v in attr.items():
             attributes.append( {"trait_type": k, "value": v} )
     return attributes
 
+def format_file_list(files):
+    f_list = []
+    for f in files:
+        f_type = None
+        if f.endswith("mp4"):
+            f_type = "video/mp4"
+        if f.endswith("png"):
+            f_type = "image/png"
+        f_list.append({"uri" : f, "type": f_type})
+    return f_list
+
+def format_royalties(creators, shares):
+    c_list = []
+    for c, s in zip(creators, shares):
+        c_list.append({
+            "address": c,
+            "share": s
+        })
+    return c_list
+
+
 def generate_metadata(
-    name, 
-    symbol, 
-    description, 
+    name,
+    royalties, # generated with format_royalty_list
+    collection_name='',
+    symbol='',
+    description='',
+    image_path='',
+    animation_path='',
     external_url='homunculi.org/art',
+    attributes=[],
+    files=[],
+    category='image',
+    collection_family='cymatic-cyborgues',
     seller_fee_basis_points=0):
 
+    file_list = format_file_list(files)
 
     metadata = {
-        "name": "Solflare X NFT",
-        "symbol": "",
-        "description": "Celebratory Solflare NFT for the Solflare X launch",
-        "seller_fee_basis_points": 0,
-        "image": "https://www.arweave.net/abcd5678?ext=png",
-        "animation_url": "https://www.arweave.net/efgh1234?ext=mp4",
-        "external_url": "https://solflare.com",
-        "attributes": [
-            {
-            "trait_type": "web",
-            "value": "yes"
-            },
-            {
-            "trait_type": "mobile",
-            "value": "yes"
-            },
-            {
-            "trait_type": "extension",
-            "value": "yes"
-            }
-        ],
+        "name": name,
+        "symbol":symbol,
+        "description": description,
+        "seller_fee_basis_points": seller_fee_basis_points,
+        "image": image_path,
+        "animation_url": animation_path,
+        "external_url": external_url,
+        "attributes": attributes,
+
         "collection": {
-            "name": "Solflare X NFT",
-            "family": "Solflare"
+            "name": collection_name,
+            "family": collection_family
         },
         "properties": {
-            "files": [
-            {
-                "uri": "https://www.arweave.net/abcd5678?ext=png",
-                "type": "image/png"
-            },
-            {
-                "uri": "https://watch.videodelivery.net/9876jkl",
-                "type": "unknown",
-                "cdn": true
-            },
-            {
-                "uri": "https://www.arweave.net/efgh1234?ext=mp4",
-                "type": "video/mp4"
-            }
-            ],
-            "category": "video",
-            "creators": [
-            {
-                "address": "xEtQ9Fpv62qdc1GYfpNReMasVTe9YW5bHJwfVKqo72u",
-                "share": 100
-            }
-            ]
+            "files": file_list,
+            "category": category,
+            "creators": royalties
         }
-        }
+    }
+    return metadata
 
-    for k, v in dict_attrs.items():
-        metadata
+if __name__ == "__main__":
+    metadata = generate_metadata(
+        name,
+        royalties, # generated with format_royalty_list
+        collection_name='',
+        symbol='',
+        description='',
+        image_url='',
+        animation_url='',
+        external_url='homunculi.org/art',
+        attributes=[],
+        files=[],
+        category='image',
+        collection_family='cymatic-cyborgues',
+        seller_fee_basis_points=0)
+
+# "attributes": [
+#     {
+#     "trait_type": "web",
+#     "value": "yes"
+#     },
+#     {
+#     "trait_type": "mobile",
+#     "value": "yes"
+#     },
+#     {
+#     "trait_type": "extension",
+#     "value": "yes"
+#     }
+# ],
+
+# "files": [
+# {
+#     "uri": "https://www.arweave.net/abcd5678?ext=png",
+#     "type": "image/png"
+# },
+# {
+#     "uri": "https://www.arweave.net/efgh1234?ext=mp4",
+#     "type": "video/mp4"
+# }
+# ]
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import shutil
+import os
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
@@ -17,5 +19,10 @@ def load_coco_categories(path="annotations/instances_val2017.json"):
     return categories_id
 
 def save_json(path, data):
-    with open(path, 'w') as outfile:
-        json.dump(data, outfile)
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def copy_file(source, new_directory):
+    destination = os.path.join(new_directory, os.path.split(source)[-1])
+    shutil.copyfile(source, destination)
+    print(f'copied {source} to {destination}')
